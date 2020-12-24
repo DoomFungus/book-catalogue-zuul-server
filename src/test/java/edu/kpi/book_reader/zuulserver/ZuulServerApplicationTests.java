64a56fe99fdab0eacec9aec01eb324b7b144b2b2
DoomFungus <unspeakableferretofdoom@gmail.com>
2020-12-24 13:19:12 +0200

    Initial commit

---
+++ src/test/java/edu/kpi/book_reader/zuulserver/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kpi.book_reader.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
